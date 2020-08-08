59e07f79f6f685ccc8dd685c957004f8ca574b61
Lopes-7 <gui-lopes7@live.com>
2020-08-08 20:45:46 -0300

    Use welcome page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
+/**
+ * IMPORTS
+ */
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import {Redirect} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import {Welcome} from './pages/welcome.js'; 
+
+
+/**
+ * STYLES
+ */
 import './App.css';
 
+
+/**
+ * CODE
+ */
 function App() {
   return (
     <div className="App">
-      <header>Spotifood</header>
+				<BrowserRouter>
+					<Switch>
+						<Redirect path="/authorized" to="/home" />
+						<Route path="/" exact component={Welcome} />
+						<Route path="/home" />
+					</Switch>
+				</BrowserRouter>
     </div>
   );
 }
 
+
+/**
+ * EXPORTS
+ */
 export default App;
