b43f7d8c9ca7d0cec9c61a2ddfa5aacbea54c698
Lopes-7 <gui-lopes7@live.com>
2020-08-09 17:25:14 -0300

    Add home page

---
+++ src/pages/home.js
@@ -0,0 +1,28 @@
+/**
+ * IMPORTS
+ */
+import React from 'react';
+import {Sidebar} from '../components/sidebar.js';
+
+
+/**
+ * STYLES
+ */
+
+
+/**
+ * CODE
+ */
+function Home () {
+
+    return (
+        <div>
+            <Sidebar />
+        </div>
+    )
+}
+
+/**
+ * EXPORTS
+ */
+export {Home};
