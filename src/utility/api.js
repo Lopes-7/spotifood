1f5c0ac06a874f30db4ae0f8b2a57aa52008babe
Lopes-7 <gui-lopes7@live.com>
2020-08-11 11:43:51 -0300

    Add utility spotifyApi

--- src/utility/api.js
+++ src/utility/api.js
@@ -11,8 +11,17 @@ const api = axios.create({
     baseURL: 'http://www.mocky.io/v2/5a25fade2e0000213aa90776'
 });
 
+const token = localStorage.getItem('access_token');
+const type = localStorage.getItem('token_type');
+
+const spotifyApi = axios.create({
+    baseURL: 'https://api.spotify.com/v1/',
+    headers: {'Authorization': type + ' ' + token}
+});
 
 /**
  * EXPORTS
  */
-export {api};
+export {
+    api,
+    spotifyApi};
