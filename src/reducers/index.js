77002c255550dd1eb6c1db0024376e3471c67c57
Lopes-7 <gui-lopes7@live.com>
2020-08-11 09:40:19 -0300

    Add global state search

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,15 @@
  */
 import {combineReducers} from 'redux';
 import {filterReducer as filters} from './filterreducer.js';
+import {searchReducer as search} from './searchreducer.js';
 
 
 /**
  * CODE
  */
 const reducers = combineReducers({
-    filters
+    filters,
+    search
 });
 
 
