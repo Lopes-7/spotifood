699a1a82230fefa80d89d876558bd42a9f81ac3d
Lopes-7 <gui-lopes7@live.com>
2020-08-12 00:13:39 -0300

    Use alerts

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {transitions, Provider as AlertProvider} from 'react-alert'
+import AlertTemplate from 'react-alert-template-basic'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom';
 
+const alertOptions = {
+  offset: '30px',
+  transition: transitions.SCALE,
+  timeout: 5000
+};
+
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AlertProvider template={AlertTemplate} {...alertOptions}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AlertProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
